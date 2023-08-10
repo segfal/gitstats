9e2d66c9f04d28d9935cfccf63b076e400ed77d5
Unknown-Pplayer <xwang2230@gmail.com>
2023-08-10 15:55:22 -0400

    setting up all fragment

--- frontend/src/components/GeneralInfo.jsx
+++ frontend/src/components/GeneralInfo.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import moment from 'moment'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import moment from 'moment';
 
-const GeneralInfo = ({ghUrl}) => {
-    
-    /*
+const GeneralInfo = ({ ghUrl }) => {
+  /*
     Use ghUrl for general info
     Do an axios call on ghUrl to retrieve data like repo_name, description, created at, updated at 
 
@@ -19,14 +18,14 @@ const GeneralInfo = ({ghUrl}) => {
     
     
     */
-   const [repoName, setRepoName] = useState('');
-   const [description, setDescription] = useState('');
-   const [dateCreated, setDateCreated] = useState('');
-   const [dateUpdated, setDateUpdated] = useState('')
+  const [repoName, setRepoName] = useState('');
+  const [description, setDescription] = useState('');
+  const [dateCreated, setDateCreated] = useState('');
+  const [dateUpdated, setDateUpdated] = useState('');
 
-   const[contributors, setContributors] = useState([{}]);
-   
-   useEffect(() => {
+  const [contributors, setContributors] = useState([{}]);
+
+  useEffect(() => {
     async function fetchRepoInfo() {
       try {
         const response = await axios.get(ghUrl);
@@ -39,33 +38,50 @@ const GeneralInfo = ({ghUrl}) => {
         console.log(error);
       }
     }
-    fetchRepoInfo()
-   }, []);
+    fetchRepoInfo();
+  }, []);
 
-   useEffect(() => {
-      async function fetchContributors() {
-        try {
-            const response = await axios.get(`${ghUrl}/contributors`)
-            console.log(response.data)
-            setContributors(response.data)
-        } catch (error) {
-            console.log(error)
-        }
+  useEffect(() => {
+    async function fetchContributors() {
+      try {
+        const response = await axios.get(`${ghUrl}/contributors`);
+        console.log(response.data);
+        setContributors(response.data);
+      } catch (error) {
+        console.log(error);
       }
-      fetchContributors();
-   }, [])
-   
+    }
+    fetchContributors();
+  }, []);
+  console.log('From useState', contributors);
+
+  const totalContributions = contributors.reduce((acc, curr) => {
+    return acc + curr.contributions;
+  }, 0);
+
   return (
     <div>
-         <h1>General Info</h1>
-         <h1>{repoName}</h1>
-         <p>Description: {description}</p>
-         <p>Date Created: {moment(dateCreated).format('YYYY-MM-DD, h:mm:ss a')}</p>
-         <p>Date Updated: {moment(dateUpdated).format('YYYY-MM-DD, h:mm:ss a')}</p>
-         <p></p>
+      <h1>General Info</h1>
+      <h1>{repoName}</h1>
+      <p>Description: {description}</p>
+      <p>Date Created: {moment(dateCreated).format('YYYY-MM-DD, h:mm:ss a')}</p>
+      <p>Date Updated: {moment(dateUpdated).format('YYYY-MM-DD, h:mm:ss a')}</p>
+      <p></p>
+      <h2>Total Commits: {totalContributions}</h2>
+      <h2>Contributors:</h2>
+      {contributors.map((contributor, i) => {
+        return (
+          <div key={i}>
+            <li>{contributor.login}</li>
+            <li>{contributor.contributions}</li>
+            <li>
+              <img src={contributor.avatar_url} alt="avatar" />
+            </li>
+          </div>
+        );
+      })}
     </div>
-   
-  )
-}
+  );
+};
 
-export default GeneralInfo
+export default GeneralInfo;
