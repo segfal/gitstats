a085d8ada086bbdc94da0e5dd5e0bdb1c78a0e8f
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-08-11 18:10:29 -0400

    Added useEffect to make sure the data arrives
    Co-authored-by: Huda Ayaz <huda-ayaz@users.noreply.github.com>

--- frontend/src/components/TimeToMerge.jsx
+++ frontend/src/components/TimeToMerge.jsx
@@ -1,11 +1,23 @@
 import React,{useState,useEffect} from 'react'
 import moment from 'moment'
 import axios from 'axios'
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    LineChart,
+    Line,
+  } from 'recharts';
 
 const TimeToMerge = ({submit,userName, repoName}) => {
     ///pull request took x hours to merge
     const [timeToMerge, setTimeToMerge] = useState(0)
     const [username, setUsername] = useState('');
+    const [mergeArray, setMergeArray] = useState([]);
 
     const PullSearch = async (repoName, userName) => {
         const response = await axios.get(`https://api.github.com/search/issues?q=repo:${userName}/${repoName}/+is:pr+is:merged`);
@@ -16,7 +28,7 @@ const TimeToMerge = ({submit,userName, repoName}) => {
     }
 
     ///https://api.github.com/repos/segfal/KaraokeApp
-    const ComputeTime = async () => {
+    const computeTime = async () => {
         let sum = 0;
         //https://api.github.com/search/issues?q=repo:segfal/karaokeapp/+is:pr+is:merged
         let pullInfo = await PullSearch(repoName, userName);
@@ -26,19 +38,31 @@ const TimeToMerge = ({submit,userName, repoName}) => {
             let createdAt = await moment(pullInfo[i].created_at);
             let mergedAt = await moment(pullInfo[i].pull_request.merged_at);
             // calculate the difference between the two
-            let difference = await mergedAt.diff(createdAt, 'hours'); 
+            let difference = await mergedAt.diff(createdAt, 'seconds'); 
+            setMergeArray([...mergeArray, difference]);
             // add the difference to sum
             sum += difference;
         }
         setTimeToMerge(sum / pullInfo.length);
         return await timeToMerge;
     }
+
+    useEffect(() => {
+        if(submit){
+            computeTime();
+        }
+
+    },[submit]);
     
     if (submit) {
         return (
             <div>
                 
-                <h2>On average, pull requests are in review for {console.log("COMPUTE TIME DATA",ComputeTime())} hours</h2>
+                <h2>On average, pull requests are in review for {timeToMerge} seconds</h2>
+                <h2>On average, pull requests are in review for {timeToMerge/60} minutes</h2>
+                <h2>On average, pull requests are in review for {timeToMerge/3600} hours</h2>
+                <h2>Unreviewed pr time chart Data</h2>
+
             </div>
         ) 
     }
