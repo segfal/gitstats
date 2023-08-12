3cd169a7b7f854cf2e82dfe735ddce6c3479bdd8
9Mohamedismail <9Mohamedismail@gmail.com>
2023-08-11 21:10:40 -0400

    Implemented a simple-default Pie Chart to visualize UnReviewedPR.

--- frontend/src/components/UnreviewedPR.jsx
+++ frontend/src/components/UnreviewedPR.jsx
@@ -1,57 +1,158 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { ResponsivePie } from "@nivo/pie";
 
-const UnreviewedPR = ({userName, repoName, access_token}) => {
-    const [allMergedPR, setAllMergedPR] = useState(0);
-    const [uncommentedPR, setUncommentedPR] = useState(0);
+const UnreviewedPR = ({ userName, repoName, access_token }) => {
+  const [allMergedPR, setAllMergedPR] = useState(0);
+  const [uncommentedPR, setUncommentedPR] = useState(0);
+  // colors dont work hahahhahahah
+  const [pieData, setPieData] = useState([
+    {
+      id: "reviewed",
+      label: "Reviewed",
+      value: 0,
+      color: "hsl(337, 70%, 50%)",
+    },
+    {
+      id: "unreviewed",
+      label: "Unreviewed",
+      value: 0,
+      color: "hsl(120, 70%, 50%)",
+    },
+  ]);
 
-    const fetchAllMergedPR = async () => {
-        try {
-            const response = await axios.get(
-              `https://api.github.com/search/issues?q=repo:${userName}/${repoName}/+is:pr+is:merged`, 
-              {
-                headers: {
-                    Authorization: "Bearer" + access_token,
-                },
-              }
-            );
-            setAllMergedPR(response.data.total_count);
-        } catch (error) {
-            console.log(error);
+  const fetchAllMergedPR = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/search/issues?q=repo:${userName}/${repoName}/+is:pr+is:merged`,
+        {
+          headers: {
+            Authorization: "Bearer" + access_token,
+          },
         }
+      );
+      setAllMergedPR(response.data.total_count);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const fetchUncommentedPR = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.github.com/search/issues?q=repo:${userName}/${repoName}/+is:pr+is:merged+comments:0`,
-          {
-            headers: {
-              Authorization: "Bearer" + access_token,
+  const fetchUncommentedPR = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/search/issues?q=repo:${userName}/${repoName}/+is:pr+is:merged+comments:0`,
+        {
+          headers: {
+            Authorization: "Bearer" + access_token,
+          },
+        }
+      );
+      setUncommentedPR(response.data.total_count);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // (Number of PRs with no comments / Total number of merged PRs) * 100
+  const getUnreviewedPRPercentage = () => {
+    return (uncommentedPR / allMergedPR) * 100;
+  };
+
+  useEffect(() => {
+    fetchAllMergedPR();
+    fetchUncommentedPR();
+  }, [userName, repoName]);
+
+  useEffect(() => {
+    setPieData((prevState) => [
+      {
+        ...prevState[0],
+        value: (allMergedPR / 100).toFixed(1),
+      },
+      {
+        ...prevState[1],
+        value: getUnreviewedPRPercentage().toFixed(1),
+      },
+    ]);
+  }, [allMergedPR, uncommentedPR]);
+
+  return (
+    <>
+      <h1>UnreviewedPR</h1>
+      <p>
+        Percentage of unreviewed pull requests: {getUnreviewedPRPercentage()}
+      </p>
+      <div style={{ height: 500 }}>
+        <ResponsivePie
+          data={pieData}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 0.2]],
+          }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: "color" }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{
+            from: "color",
+            modifiers: [["darker", 2]],
+          }}
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              size: 4,
+              padding: 1,
+              stagger: true,
             },
-          }
-        );
-        setUncommentedPR(response.data.total_count);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    const getUnreviewedPRPercentage = () => {
-        return uncommentedPR / allMergedPR;
-    }
-
-    useEffect(() => {
-        fetchAllMergedPR();
-        fetchUncommentedPR();
-    }, [userName, repoName]);
-
-    return (
-      <>
-        <h1>UnreviewedPR</h1>
-        <p>Percentage of unreviewed pull requests: {getUnreviewedPRPercentage()}</p>
-      </>
-    );
-}
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          legends={[
+            {
+              anchor: "bottom",
+              direction: "row",
+              justify: false,
+              translateX: 0,
+              translateY: 56,
+              itemsSpacing: 0,
+              itemWidth: 100,
+              itemHeight: 18,
+              itemTextColor: "#999",
+              itemDirection: "left-to-right",
+              itemOpacity: 1,
+              symbolSize: 18,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
+};
 
 export default UnreviewedPR;
