ed0eb717f21a1a9cebd04085241f1c2fb509ad82
9Mohamedismail <9Mohamedismail@gmail.com>
2023-08-17 00:51:55 -0400

    Complete landingpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,18 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from "react";
 
 import "./App.css";
 import LandingPage from "./components/LandingPage";
 import ParticleBackground from "./components/ParticleBackground";
-import RepoDataPage from "./components/RepoDataPage";
 
 const CLIENT_ID = "9dfb3cba168ba38c3d35";
 
 function App() {
   return (
-    <div>
-      <div id="particles-js">
+    <div id="particles-js">
+      <div>
         <ParticleBackground />
         <LandingPage />
       </div>
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/repo" element={<RepoDataPage />} />
-      </Routes>
-
-      {/*  <button onClick={getUserRateLimit}>(Test)Get User Rate Limit</button>
-      <button onClick={getRateLimit}>(Test)Get IP Rate Limit</button> */}
     </div>
   );
 }
--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import LogoutButton from "./githubOAuth/LogoutButton";
 import GitHubButton from "./githubOAuth/GithubButton";
-import { useNavigate } from "react-router-dom";
-import RepoDataPage from "./RepoDataPage"; // Import RepoDataPage
+import LogoutButton from "./githubOAuth/LogoutButton";
+import GeneralInfo from "./GeneralInfo";
+import DeploymentFreq from "./DeploymentFreq";
+import UnreviewedPR from "./UnreviewedPR";
+import TimeToMerge from "./TimeToMerge";
 import axios from "axios";
 import "../stylesheets/landingCSS.css";
 
@@ -17,10 +19,9 @@ function LandingPage() {
   const [repoUrl, setRepoUrl] = useState("");
   const [userName, setUserName] = useState("");
   const [repoName, setRepoName] = useState("");
+  // const [ghUrl, setGhUrl] = useState('');
   const [submit, setSubmit] = useState(false);
-  const navigate = useNavigate();
   const ghUrl = `https://api.github.com/repos/${userName}/${repoName}`;
-
   useEffect(() => {
     console.log(repoUrl);
   }, [repoUrl]);
@@ -31,18 +32,11 @@ function LandingPage() {
     setUserName(parts[parts.length - 2]);
     setRepoName(parts[parts.length - 1]);
     setSubmit(true);
-    navigate("./repo", {
-      state: {
-        userName: userName,
-        repoName: repoName,
-        ghUrl: ghUrl,
-      },
-    });
   };
 
   const loginWithGithub = () => {
     window.location.assign(
-      `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+      `https://github.com/login/oauth/authorize?scope=user&client_id=${CLIENT_ID}`
     );
   };
 
@@ -52,9 +46,12 @@ function LandingPage() {
   };
 
   useEffect(() => {
+    // access token stored in local storage for now
+    // http://localhost:5173/?code=46052fed06f20abbef61
     const query = window.location.search;
     const urlParams = new URLSearchParams(query);
     const codeParam = urlParams.get("code");
+    console.log(codeParam);
 
     if (codeParam && !localStorage.getItem("accessToken")) {
       const getAccessToken = async () => {
@@ -62,6 +59,7 @@ function LandingPage() {
           const response = await axios.get(
             `http://localhost:4000/api/login/getAccessToken?code=${codeParam}`
           );
+          console.log(response.data.access_token);
           localStorage.setItem("accessToken", response.data.access_token);
           setRerender(!rerender);
         } catch (error) {
@@ -72,27 +70,66 @@ function LandingPage() {
     }
   }, []);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:4000/api/login/getUserData`,
-          {
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem("accessToken"),
-            },
-          }
-        );
-        setUserData(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const getUserData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/api/login/getUserData`,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+        }
+      );
+      console.log(response.data);
+      setUserData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const getUserRateLimit = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/rate_limit`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getRateLimit = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/rate_limit`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
     if (localStorage.getItem("accessToken")) {
       getUserData();
     }
-  }, []);
+  }, [localStorage.getItem("accessToken")]);
+
+  // const repoSearch = async() => {
+  //   const response = await fetch(`${ghUrl}${userName}/${repoUrl}`);
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
+  //useEffect for loading the user's repo upon entering input
+  // useEffect(() =>{
+  // const repoSearch =  async() => {
+  //     // Parse useName and
+  //    // const response = await fetch(ghUrl + userName + '/' + repoUrl);
+  // // const data = await response.json();
+  // // console.log(data);
+
+  // }
 
   return (
     <div>
@@ -109,9 +146,11 @@ function LandingPage() {
               <GitHubButton loginWithGithub={loginWithGithub} />
             )}
           </nav>
-          <h1 className="userGreeting">
-            Welcome to your GitStats account, {userName}!
-          </h1>
+          {localStorage.getItem("accessToken") && (
+            <h1 className="userGreeting">
+              Welcome to your GitStats account, {userData.userName}!
+            </h1>
+          )}
           <div className="landingTitle">
             <h1>GitHub Stats</h1>
           </div>
@@ -142,8 +181,59 @@ function LandingPage() {
           </div>
         </div>
       ) : (
-        <RepoDataPage />
+        <div className="All_Components_Box">
+          {submit && <GeneralInfo ghUrl={ghUrl} />}
+
+          {localStorage.getItem("accessToken") && submit && (
+            <TimeToMerge
+              submit={submit}
+              userName={userName}
+              repoName={repoName}
+              access_token={localStorage.getItem("accessToken")}
+            />
+          )}
+
+          {localStorage.getItem("accessToken") && submit && (
+            <UnreviewedPR
+              userName={userName}
+              repoName={repoName}
+              access_token={localStorage.getItem("accessToken")}
+            />
+          )}
+
+          {localStorage.getItem("accessToken") && submit && (
+            <DeploymentFreq
+              ghUrl={ghUrl}
+              access_token={localStorage.getItem("accessToken")}
+            />
+          )}
+        </div>
       )}
+
+      {localStorage.getItem("accessToken") && !submit ? (
+        <div>
+          <h1 className="landingMessageBottom">Your Recent Repositories</h1>
+        </div>
+      ) : (
+        <div>
+          <h1 className="landingMessageBottom">
+            To view more stats and personalized information, login in with your
+            Github account
+          </h1>
+        </div>
+      )}
+
+      {!localStorage.getItem("accessToken") ? (
+        <div className="githubLogin">
+          <GitHubButton loginWithGithub={loginWithGithub} />
+        </div>
+      ) : (
+        <></>
+      )}
+
+      {/* Buttons for testing 
+      <button onClick={getUserRateLimit}>(Test)Get User Rate Limit</button>
+      <button onClick={getRateLimit}>(Test)Get IP Rate Limit</button> */}
     </div>
   );
 }
