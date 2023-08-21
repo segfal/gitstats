17b1c7bbf9e69feceb4f93be22903ae238739413
Huda Ayaz <hudaayaz21@gmail.com>
2023-08-21 01:06:50 -0400

    Progress on recent repos, fetch not working yet

--- frontend/src/components/RecentRepoCard.jsx
+++ frontend/src/components/RecentRepoCard.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import axios from "axios";
+import moment from "moment";
+import "../stylesheets/RecentRepo.css";
+import "../stylesheets/All_Components.css";
 
-const RecentRepoCard = () => {
+const RecentRepoCard = ({repo}) => {
+    const repoName = repo.name;
+    const description = repo.description;
+    const dateCreated = repo.created_at;
+    const dateUpdated = repo.updated_at;
+
+    // onClick for "View Stats" button should lead to page of stats with the api link passed using "full_name" passed from the "repo" prop as: https://api.github.com/repos/${repo.full_name}
+    // otherwise, could replace the "View Stats" button with a "Copy Repo Link" button so that the repo link can be pasted into the input box and processed normally
     
+    return (
+        <div className="card_container">
+            <h1 className="repo-name">{repoName}</h1>
+            <h2>Created on {moment(dateCreated).format("MMMM Do YYYY")}</h2>
+            <h2>Updated on {moment(dateUpdated).format("YMMMM Do YYYY")}</h2>
+            <p className="desc">Description: {description}</p>
+            <button className="view-stats">VIEW STATS</button>
+        </div>
+    )
 }
 
 export default RecentRepoCard;
--- frontend/src/components/RecentRepos.jsx
+++ frontend/src/components/RecentRepos.jsx
@@ -1,8 +1,46 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
+import RecentRepoCard from './RecentRepoCard';
+import "../stylesheets/landingCSS.css";
+import "../stylesheets/RecentRepo.css";
+import "../stylesheets/All_Components.css";
 
-const RecentRepos = () => {
-    <h1>Recent Repositories</h1>
+const RecentRepos = ({userName, access_token}) => {
+    const [userRepos, setUserRepos] = useState();
+    const fetchRecentRepos = async () => {
+        try {
+          const response = await axios.get(
+            `https://api.github.com/users/${userName}/repos?sort=updated&per_page=4`,
+            {
+              headers: {
+                Authorization: "Bearer " + access_token,
+              },
+            }
+          );
+          setUserRepos(response.data);
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchRecentRepos();
+    }, [userRepos]);
+
+    return ( 
+        <div>
+            <h1 className='landingMessageBottom'>Your Recent Repositories</h1>
+            <div className='repos_container'>
+                <RecentRepoCard repo={userRepos[1]}/>
+                <RecentRepoCard repo={userRepos[2]}/>
+            </div>
+            <div className='repos_container'>
+                <RecentRepoCard repo={userRepos[3]}/>
+                <RecentRepoCard repo={userRepos[4]}/>
+            </div>
+        </div>
+    )
 }
 
 export default RecentRepos;
