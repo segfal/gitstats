c557e1e6561caed289c8db016e8cc2d99678c337
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-08-23 01:20:54 -0400

    commit implementation

---
+++ frontend/src/components/NewsFeed.jsx
@@ -0,0 +1,49 @@
+///news feed to showcase all commited changes
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+import "../stylesheets/PRImpact.css";
+import "../stylesheets/All_Components.css";
+
+
+
+const NewsFeed = ({submit,userName, repoName, access_token}) => {
+    //Commit message array
+    const [commitMessage, setCommitMessage] = useState([]);
+    //Commit author array
+    const [commitAuthor, setCommitAuthor] = useState([]);
+    //Commit date array
+    const [commitDate, setCommitDate] = useState([]);
+    //Commit url array
+    const [commitUrl, setCommitUrl] = useState([]);
+    //Commit repo name array
+    const [commitRepoName, setCommitRepoName] = useState([]);
+    //commit author avatar array
+    const [commitAuthorAvatar, setCommitAuthorAvatar] = useState([]);
+    
+
+    //Get all commits from repo
+    const getCommits = async (Repo, User) => {
+
+        let commits = [];
+        const response = await axios.get(
+            `https://api.github.com/repos/${User}/${Repo}/commits?per_page=10`,
+            {
+              headers: {
+                Authorization: "Bearer " + access_token,
+              },
+            }
+        );
+        
+        
+
+    }
+
+
+}
+
+
+export default NewsFeed;
+
+
