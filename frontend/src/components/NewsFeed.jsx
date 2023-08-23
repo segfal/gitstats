21cffcbfe5fb8ef2bb1fc5eae790ea12d678ad7d
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-08-23 13:12:52 -0400

    commit implement

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -35,10 +35,38 @@ const NewsFeed = ({submit,userName, repoName, access_token}) => {
               },
             }
         );
-        
-        
-
+        commits = response.data;
+        return commits;
     }
+
+    //Get all commit messages from repo
+    useEffect(() => {
+
+        const getCommitMessages = async () => {
+            let commits = await getCommits(repoName, userName);
+            let commitMessages = [];
+            let commitAuthors = [];
+            let commitDates = [];
+            let commitUrls = [];
+            let commitRepoNames = [];
+            let commitAuthorAvatars = [];
+            for (let i = 0; i < commits.length; i++) {
+                commitMessages.push(commits[i].commit.message);
+                commitAuthors.push(commits[i].commit.author.name);
+                commitDates.push(commits[i].commit.author.date);
+                commitUrls.push(commits[i].html_url);
+                commitRepoNames.push(commits[i].repository.name);
+                commitAuthorAvatars.push(commits[i].author.avatar_url);
+            }
+            setCommitMessage(commitMessages);
+            setCommitAuthor(commitAuthors);
+            setCommitDate(commitDates);
+            setCommitUrl(commitUrls);
+            setCommitRepoName(commitRepoNames);
+            setCommitAuthorAvatar(commitAuthorAvatars);
+        }
+        getCommitMessages();
+    })
 
 
 }
