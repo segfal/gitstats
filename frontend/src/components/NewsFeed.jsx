71c7830ca33cb3bae3fe635784065c17abd2f5a8
segfaultboi@gmail.com <segfaultboi@gmail.com>
2023-08-24 13:37:48 -0400

    correction on NewsFeed.css

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
-import "../stylesheets/newsFeed.css";
+import "../stylesheets/NewsFeed.css";
 import "../stylesheets/All_Components.css";
 
 
